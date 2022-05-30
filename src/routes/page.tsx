b6d9b4bdd8812dfce1b89cccffdc0d3b771ec0b6
sieem <siem.linx@gmail.com>
2022-05-30 18:59:55 +0200

    Document title is for later

--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -3,8 +3,6 @@ import { useParams } from 'react-router-dom';
 export function Page() {
   const { pageIdEven, pageIdOdd } = useParams();
 
-  document.title = `OOH [${pageIdEven}-${pageIdOdd}]`;
-
   return <main className="p-2">
     <h2>{pageIdEven}-{pageIdOdd}</h2>
   </main>;
