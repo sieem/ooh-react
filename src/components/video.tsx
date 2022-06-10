70951600ce5f20c1fd090b06e5ea61dc901b0e07
sieem <siem.linx@gmail.com>
2022-06-10 22:17:49 +0200

    Smooooooth video 🥳

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -25,41 +25,33 @@ export function Video({ videoForward, videoBackward, pageId }: { videoForward: s
     
   }, [currentLeftPage, currentRightPage]);
 
-  const playForward = () => {
-    if (!videoForwardRef.current || !videoBackwardsRef.current) {
+
+  const switchVideo = (prevVideo: HTMLVideoElement | null, nextVideo: HTMLVideoElement | null, nextPosition: 'forward' | 'backward') => {
+    if (!prevVideo || !nextVideo) {
       return;
     }
 
-    if (videoBackwardsRef.current.currentTime === 0) {
-      videoBackwardsRef.current.currentTime = videoBackwardsRef.current.duration;
+    nextVideo.currentTime = prevVideo.duration - prevVideo.currentTime;
+
+    const update = () => {
+      setVideoPosition(nextPosition);
+      nextVideo?.play();
+      nextVideo?.removeEventListener('timeupdate', update);
     }
 
-    videoForwardRef.current.currentTime = videoBackwardsRef.current.duration - videoBackwardsRef.current.currentTime;
+    // Using the event listener as a promised way to know when the currentTime is applied
+    nextVideo?.addEventListener('timeupdate', update);
+  }
 
-    setTimeout(() => {
-      videoForwardRef.current?.play();
-      setVideoPosition('forward');
-    });
-  };
+  const playForward = () => void switchVideo(videoBackwardsRef.current, videoForwardRef.current, 'forward');
+  const playBackward = () => void switchVideo(videoForwardRef.current, videoBackwardsRef.current, 'backward');
 
-  const playBackward = () => {
-    if (!videoForwardRef.current || !videoBackwardsRef.current) {
-      return;
-    }
-    videoBackwardsRef.current.currentTime = videoForwardRef.current.duration - videoForwardRef.current.currentTime;
-
-    setTimeout(() => {
-      videoBackwardsRef.current?.play();
-      setVideoPosition('backward');
-    });
-  };
-  
-  const forwardStyle = videoPosition === 'forward' ? { opacity: '1' } : {};
-  const backwardStyle = videoPosition === 'backward' ? { opacity: '1' } : {};
+  const forwardStyle = videoPosition === 'forward' ? { opacity: '1' } : { };
+  const backwardStyle = videoPosition === 'backward' ? { opacity: '1' } : { };
 
   return <div className='h-full w-full relative'>
     <video className='opacity-0 absolute h-full w-full' style={forwardStyle} muted preload="auto" src={`${baseUrl}/${videoForward}`} ref={videoForwardRef} ></video>
     <video className='opacity-0 absolute h-full w-full' style={backwardStyle} muted preload="auto" src={`${baseUrl}/${videoBackward}`} ref={videoBackwardsRef} ></video>
-    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2' onMouseEnter={playForward} onMouseLeave={playBackward}></div>
+    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-1/4 h-1/2' onMouseEnter={playForward} onMouseLeave={playBackward}></div>
   </div>
 }
